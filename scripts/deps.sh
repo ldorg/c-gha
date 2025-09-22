9a3ae34f90fffd50b3e923c25fb942d1b428c707
Logan Donley <ldonley@cloudbees.com>
2025-09-22 13:40:37 -0400

    fixing deps

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -16,10 +16,20 @@ sudo apt-get install -y \
     cppcheck \
     clang-format
 
+echo "Installing Python and Unity test dependencies"
+sudo apt-get install -y \
+    python3 \
+    python3-pip
+
+# Install Unity dependencies 
+pip3 install --break-system-packages pyparsing junit-xml
+
 echo "Checking versions:"
 cmake --version | head -n1
 gcc --version | head -n1
 cppcheck --version
 clang-format --version
+python3 --version
+pip3 --version
 
 echo "Done."
